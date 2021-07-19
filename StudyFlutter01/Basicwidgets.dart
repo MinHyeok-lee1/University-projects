2804c49f3af719e446cbb91bf161d1e00c6c1e28
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-07-19 11:37:16 +0900

    Add files via upload

---
+++ StudyFlutter01/Basicwidgets.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Appbar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Appbar icon menu'),
+          centerTitle: true,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: null,
+            tooltip: "Navigation menu",
+          ),
+          actions: <Widget>[
+            IconButton(
+              tooltip: "Search",
+              onPressed: null,
+              icon: Icon(Icons.search)),
+          ],
+        ),
+        body: Container(
+          child: Center(
+            child: Text("Hello, world!"),
+          ),
+        ),
+    );
+  }
+}
