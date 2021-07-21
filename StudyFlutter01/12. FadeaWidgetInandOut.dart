6c427e0c1ba122eb1c1c0c5e26daf48dd6b55fc5
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-07-21 09:41:28 +0900

    Update 12. FadeaWidgetInandOut.dart

--- StudyFlutter01/12. FadeaWidgetInandOut.dart
+++ StudyFlutter01/12. FadeaWidgetInandOut.dart
@@ -40,19 +40,28 @@ class _MyHomePageState extends State<MyHomePage>{
         title: Text(widget.title),
       ),
       body: Center(
+        // 투명도 애니메이션 효과를 제공하는 위젯 추가
         child: AnimatedOpacity(
+          // 필드값에 따라 출력 여부를 토글
           opacity: _visible ? 1.0 : 0.0,
+          // 애니메이션 효과에 소요되는 시간 설정
           duration: Duration(milliseconds: 500),
+          // 컨테이너 추가
           child: Container(
+            // 컨테이너의 너비, 높이, 색상 설정
             width: 200.0,
             height: 200.0,
             color: Colors.green,
           ),
         ),
       ),
+      
+      // 플로팅액션버튼 추가
       floatingActionButton: FloatingActionButton(
         onPressed: (){
+          // 상태값 재설정
           setState(() {
+            // 컨테이너의 표시여부를 토글
             _visible = !_visible;
           });
         },
