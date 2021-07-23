a20288a2f4f891a46feed9c7ca2ee49433e39641
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-07-23 16:44:40 +0900

    Add files via upload

---
+++ StudyFlutter01/32. CreateaGridList.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    final title = 'Grid List';
+
+    return MaterialApp(
+      title: title,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: GridView.count(crossAxisCount: 2,
+        children: List.generate(100, (index) => Center(
+          child: Text(
+            'Item $index',
+            style: Theme.of(context).textTheme.headline5,
+          )
+        ))),
+      ),
+    );
+  }
+}
