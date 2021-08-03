935219e53ee9278a46e67db43c0d00ec8a27f7c8
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-08-03 13:21:57 +0900

    Add files via upload

--- StudyFlutter02/20. LayOutDemo.dart
+++ StudyFlutter02/20. LayOutDemo.dart
@@ -5,64 +5,148 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 별점표기
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        const Icon(Icons.star, color: Colors.black),
+        const Icon(Icons.star, color: Colors.black),
+      ],
+    );
+
+    // 점수
+    final ratings = Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          stars,
+          const Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    const descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+// DefaultTextStyle.merge()를 사용하면 기본 텍스트를 만들 수 있음
+// 자식과 모든 후속 자식이 상속하는 스타일
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                const Text('PREP:'),
+                const Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                const Text('COOK:'),
+                const Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                const Text('FEEDS:'),
+                const Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
     return MaterialApp(
         title: 'LayoutDemo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primaryColor: Colors.white,
         ),
+
         home: Scaffold(
           appBar: AppBar(title: Text('WOW')),
-          body:
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                Text('CENTER'),
-                Icon(
-                  Icons.star,
-                  color: Colors.red[500],
-                ),
-                Text('BOTTOM'),
-                Text('TOP'),
-                    ],
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('CENTER'),
+                  Icon(
+                    Icons.star,
+                    color: Colors.red[500],
                   ),
-          Stack(
-            alignment: const Alignment(0, -0.6),
-            children: [
-              const CircleAvatar(
-                backgroundImage: AssetImage('images/lake.jpg',),
-                radius: 100,
+                  Text('BOTTOM'),
+                  Text('TOP'),
+                ],
               ),
-              Container(
-                decoration: const BoxDecoration(
-                  color: Colors.black45,
-                ),
-                child: const Text(
-                  'Lake',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+
+              Stack(
+                alignment: const Alignment(0, -0.6),
+                children: [
+                  const CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'images/lake.jpg',
+                    ),
+                    radius: 100,
                   ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.black45,
+                    ),
+                    child: const Text(
+                      'Lake',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Image.asset(
+                  'images/lake.jpg',
+                  fit: BoxFit.cover,
                 ),
+
+              ),
+
+              Column(
+                children: <Widget>[
+                  ratings, // 이렇게 표현하는게 유지보수가 좋다
+                  iconList,
+                ],
               ),
             ],
           ),
-                Expanded(child:
-                Image.asset(
-                  'images/lake.jpg',
-                  fit: BoxFit.cover,
-                 ),
-                )
-
-              ],
-            ),
-
-        )
-    );
+        ));
   }
 }
