028a24b7b940362915e41295bea24a186b751b25
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-08-14 21:42:33 +0900

    Update 29. #P2. Follow.dart

--- StudyFlutter02/29. #P2. Follow.dart
+++ StudyFlutter02/29. #P2. Follow.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BottomNavigator Demo',
+      title: 'Follow Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
