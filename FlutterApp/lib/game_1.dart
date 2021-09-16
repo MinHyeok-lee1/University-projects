5542fa084842fa7836497235e13ff3e7cf297cbe
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-09-16 16:49:44 +0900

    Add files via upload

---
+++ FlutterApp/lib/game_1.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:time/time.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Timer Demo';
+    return MaterialApp(
+      title: title,
+      home: MyHomePage(
+        title: title,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(color: Colors.red),
+        ),
+
+        Expanded(
+          child: Container(color: Colors.blue),
+        ),
+      ],
+    )));
+  }
+}
