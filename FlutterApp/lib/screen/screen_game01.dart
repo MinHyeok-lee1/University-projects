0403a7e7442729d6c0f0be23e1a8bb81f0846bc5
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-09-27 19:15:01 +0900

    Update screen_game01.dart

--- FlutterApp/lib/screen/screen_game01.dart
+++ FlutterApp/lib/screen/screen_game01.dart
@@ -1,9 +1,12 @@
 // 1 vs 1 Faster Tap
+import 'dart:math';
+//import 'package:duobattle/screen/screen_home.dart';
+import 'dart:ui' as ui;
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:module/screen/screen_home.dart';
+import 'package:duobattle/model/timer_model.dart';
 
 class FirstGameScreen extends StatefulWidget {
-
   @override
   _FirstGameScreenState createState() => _FirstGameScreenState();
 }
@@ -12,100 +15,277 @@ class _FirstGameScreenState extends State<FirstGameScreen> {
   var colorOne = const Color(0xffff0000);
   var colorTwo = const Color(0xff0000ff);
   bool _visibility = false;
+  bool _first = true;
   var winnerText = 'The Winner is ';
   var name = '';
   int winner = 0;
+  var rnd = Random().nextInt(8) + 3;
+  late MilliTimer timer = new MilliTimer();
+
+  @override
+  void initState() {
+    timer.start();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.reset();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery
-        .of(context)
-        .size;
+    Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
     return SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: InkWell(
-                    child: Container(
-                      color: colorOne,
-                    ),
-                    onTap: () {
-                      winner = 1;
-                      name = 'Red';
-                      colorChange();
-                    },
-                  ),
+        body: mainBody(width)
+      ),
+    );
+  }
+
+  Widget mainBody(double width) {
+    if(_first){
+      _first = !_first;
+      return FutureBuilder(
+          future: Future.delayed(Duration(seconds: rnd)),
+          builder: (context, snapshot) {
+// Checks whether the future is resolved, ie the duration is over
+            if (snapshot.connectionState == ConnectionState.done)
+              return bodyPart2(width);
+            else
+              return bodyPart1(
+                  width); // Return empty container to avoid build errors
+          });
+    }
+    return bodyPart1(width);
+  }
+
+  Widget bodyPart1(double width) {
+    return Stack(
+      children: <Widget>[
+        childBody(),
+        BackdropFilter(
+          filter: ui.ImageFilter.blur(
+            sigmaX: 8.0,
+            sigmaY: 8.0,
+          ),
+          child: Opacity(
+            opacity: 0.01,
+            child: childBody(),
+          ),
+        ),
+        Visibility(
+          visible: !_visibility,
+          child: RotatedBox(
+            quarterTurns: 1,
+            child: Center(
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  fontSize: width * 0.04,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                Expanded(
-                  child: InkWell(
-                    child: Container(
-                      color: colorTwo,
-                    ),
-                    onTap: () {
-                      winner = 2;
-                      name = 'Blue';
-                      colorChange();
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Visibility(
-              visible: _visibility,
-              child: RotatedBox(
-                quarterTurns: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(winnerText+name,
-                        style: TextStyle(
-                          fontSize: width * 0.05,
-                          color: Colors.white,
-                        ),
-                      ),
-                    Padding(
-                      padding: EdgeInsets.all(width * 0.048),
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          IconButton(
-                            color: Colors.white,
-                            hoverColor: Colors.white,
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(
-                              Icons.arrow_back_sharp,
-                            ),
-                          ),
-                          IconButton(
-                            color: Colors.white,
-                            hoverColor: Colors.white,
-                            onPressed: () {
-                              setState(() {
-                                reprint();
-                              });
-                            },
-                            icon: const Icon(
-                              Icons.refresh_sharp,
-                            ),
-                          ),
-                        ],
-                      ),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    FadeAnimatedText('Ready for Touch..!'),
+                    FadeAnimatedText('Ready for Touch...'),
                   ],
                 ),
               ),
             ),
+          )
+        ),
+        Visibility(
+          visible: _visibility,
+          child: RotatedBox(
+            quarterTurns: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  winnerText + name,
+                  style: TextStyle(
+                    fontSize: width * 0.05,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.048),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    IconButton(
+                      color: Colors.white,
+                      hoverColor: Colors.white,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_sharp,
+                      ),
+                    ),
+                    IconButton(
+                      color: Colors.white,
+                      hoverColor: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          reprint();
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.refresh_sharp,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget bodyPart2(double width) {
+    return Stack(
+      children: <Widget>[
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: InkWell(
+                child: Container(
+                  child: Visibility(
+                    visible: _visibility,
+                    child: RotatedBox(
+                      quarterTurns: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            timer.saveTime(),
+                            style: TextStyle(
+                              fontSize: width * 0.03,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  color: colorOne,
+                ),
+                onTap: () {
+                  winner = 1;
+                  name = 'Red';
+                  colorChange();
+                },
+              ),
+            ),
+            Expanded(
+              child: InkWell(
+                child: Container(
+                  child: Visibility(
+                    visible: _visibility,
+                    child: RotatedBox(
+                      quarterTurns: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            timer.saveTime(),
+                            style: TextStyle(
+                              fontSize: width * 0.03,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  color: colorTwo,
+                ),
+                onTap: () {
+                  winner = 2;
+                  name = 'Blue';
+                  colorChange();
+                },
+              ),
+            ),
           ],
         ),
-      ),
+        Visibility(
+          visible: _visibility,
+          child: RotatedBox(
+            quarterTurns: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  winnerText + name,
+                  style: TextStyle(
+                    fontSize: width * 0.05,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.048),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    IconButton(
+                      color: Colors.white,
+                      hoverColor: Colors.white,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_sharp,
+                      ),
+                    ),
+                    IconButton(
+                      color: Colors.white,
+                      hoverColor: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          reprint();
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.refresh_sharp,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget childBody() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            color: Colors.redAccent,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.blueAccent,
+          ),
+        ),
+      ],
     );
   }
 
@@ -114,11 +294,14 @@ class _FirstGameScreenState extends State<FirstGameScreen> {
     colorTwo = const Color(0xff0000ff);
     winner = 0;
     _visibility = false;
+    _first = true;
+    timer.reset();
+    rnd = Random().nextInt(10) + 1;
   }
 
   void colorChange() {
     _visibility = true;
-
+    _first = false;
     setState(() {
       if (winner == 1) {
         colorTwo = const Color(0xffff0000);
