db2f431579f22b054a680eeaa4ad45e07111ac57
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-09-27 20:09:24 +0900

    Update screen_game01_winner.dart

--- FlutterApp/lib/screen/screen_game01_winner.dart
+++ FlutterApp/lib/screen/screen_game01_winner.dart
@@ -1,4 +1,4 @@
-// 1 vs 1 Faster Tap winnerOnly// 1 vs 1 Faster Tap
+// 1 vs 1 Faster Tap winnerOnly
 import 'dart:math';
 //import 'package:duobattle/screen/screen_home.dart';
 import 'dart:ui' as ui;
