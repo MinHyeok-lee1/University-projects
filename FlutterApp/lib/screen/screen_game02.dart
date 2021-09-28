7b461c9fd282457ed562628878f49770baee9578
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-09-28 17:57:42 +0900

    Add files via upload

--- FlutterApp/lib/screen/screen_game02.dart
+++ FlutterApp/lib/screen/screen_game02.dart
@@ -1,4 +1,4 @@
-// 5 vs 5 Tab1 5x2
+// ManyTouch
 import 'package:flutter/material.dart';
 
 class SecondGameScreen extends StatefulWidget {
@@ -9,27 +9,41 @@ class SecondGameScreen extends StatefulWidget {
 class _SecondGameScreenState extends State<SecondGameScreen> {
   var book = [
     const Color(0xffff0000),
-    const Color(0xffdc143c),
-    const Color(0xffb22222),
-    const Color(0xff800000),
-    const Color(0xff8b0000),
     const Color(0xff0000ff),
-    const Color(0xff0000cd),
-    const Color(0xff000080),
-    const Color(0xff00008b),
-    const Color(0xff191970),
+    Colors.white,
   ];
   var index = [
-    0,1,2,3,4,5,6,7,8,9
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    2,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
   ];
   bool _visibility = false;
+
   var winnerText = 'The Winner is ';
   var name = '';
   int winner = 0;
-
-  var score1 = 0;
-  var score2 = 0;
-
+  int baseline = 10;
+  var score1 = 10;
+  var score2 = 10;
+  var start_flag = 2;
 
   @override
   Widget build(BuildContext context) {
@@ -38,14 +52,15 @@ class _SecondGameScreenState extends State<SecondGameScreen> {
         .size;
     double width = screenSize.width;
 
-    if(score1 == 5) {
+    if(baseline == 21) {
       name = 'Red';
       _visibility = true;
     }
-    else if(score2 == 5) {
+    else if(baseline == -1) {
       name = 'Blue';
       _visibility = true;
     }
+
 
     return SafeArea(
       child: Scaffold(
@@ -56,117 +71,104 @@ class _SecondGameScreenState extends State<SecondGameScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(height: 5.0, width: 500, color: Colors.redAccent),
-                Expanded(
-                  child: Row(
+                Flexible(
+                  flex: 10,
+                  child: Column(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Flexible(
                         child: InkWell(
-                          child: Container(
-                            color: book[index[0]],
+                          child: Column(
+                            children: <Widget>[
+                              Flexible(
+                                child: Container(color: book[index[0]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[1]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[2]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[3]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[4]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[5]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[6]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[7]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[8]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[9]]),
+                              ),
+                            ],
                           ),
                           onTap: () {
                             colorChange(0);
                           },
                         ),
                       ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[1]],
-                          ),
-                          onTap: () {
-                            colorChange(1);
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[2]],
-                          ),
-                          onTap: () {
-                            colorChange(2);
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[3]],
-                          ),
-                          onTap: () {
-                            colorChange(3);
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[4]],
-                          ),
-                          onTap: () {
-                            colorChange(4);
-                          },
-                        ),
-                      ),
                     ],
                   ),
                 ),
-                Container(height: 0.01, width: 500, color: Colors.white),
-                Expanded(
-                  child: Row(
+                Flexible(
+                  flex: 1,
+                  child: Container(color: book[index[10]]),
+                ),
+                Flexible(
+                  flex: 10,
+                  child: Column(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Flexible(
                         child: InkWell(
-                          child: Container(
-                            color: book[index[5]],
+                          child: Column(
+                            children: <Widget>[
+                              Flexible(
+                                child: Container(color: book[index[11]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[12]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[13]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[14]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[15]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[16]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[17]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[18]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[19]]),
+                              ),
+                              Flexible(
+                                child: Container(color: book[index[20]]),
+                              ),
+                            ],
                           ),
                           onTap: () {
-                            colorChange(5);
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[6]],
-                          ),
-                          onTap: () {
-                            colorChange(6);
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[7]],
-                          ),
-                          onTap: () {
-                            colorChange(7);
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[8]],
-                          ),
-                          onTap: () {
-                            colorChange(8);
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        child: InkWell(
-                          child: Container(
-                            color: book[index[9]],
-                          ),
-                          onTap: () {
-                            colorChange(9);
+                            colorChange(1);
                           },
                         ),
                       ),
@@ -231,171 +233,85 @@ class _SecondGameScreenState extends State<SecondGameScreen> {
 
   void reprint() {
     index = [
-      0,1,2,3,4,5,6,7,8,9
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      2,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
     ];
-    score1 = 0;
-    score2 = 0;
+    score1 = 10;
+    score2 = 10;
     winner = 0;
+    baseline = 10;
     _visibility = false;
+    start_flag = 2;
   }
 
   void colorChange(int num) {
     // timer로 _visibiility 조정
     // _visibility = true;
+
     setState(() {
-      switch(num){
-        case 0:{
-          if(index[5] != 0){
-            if(index[0] != 0) {
-              score1++;
-              score2--;
+      switch (num) {
+        case 0:
+          {
+            if(baseline < 21){
+              if(start_flag == 0) {
+                ++baseline;
+              }
+              start_flag = 1;
+              ++baseline;
             }
-
-            index[0] = 0;
-            index[5] = 0;
-
-            score1++;
-            score2--;
-          }
-          break;
-        }
-        case 1:{
-          if(index[6] != 1) {
-            if(index[1] != 1) {
-              score1++;
-              score2--;
+            for(int i = 0; i < baseline; i++){
+              index[i] = 0;
             }
-
-            index[1] = 1;
-            index[6] = 1;
-
-            score1++;
-            score2--;
-          }
-          break;
-        }
-        case 2:{
-          if(index[7] != 2) {
-            if(index[2] != 2) {
-              score1++;
-              score2--;
+            for(int i = 20; i > baseline; i--){
+              index[i] = 1;
             }
-
-            index[2] = 2;
-            index[7] = 2;
-
-            score1++;
-            score2--;
+            break;
           }
-          break;
-        }
-        case 3:{
-          if(index[8] != 3) {
-            if(index[3] != 3) {
-              score1++;
-              score2--;
+        case 1:
+          {
+            if(baseline > -1){
+              if(start_flag == 1) {
+                --baseline;
+              }
+              start_flag = 0;
+              --baseline;
             }
-
-            index[3] = 3;
-            index[8] = 3;
-
-            score1++;
-            score2--;
-          }
-          break;
-        }
-        case 4:{
-          if(index[9] != 4) {
-            if(index[4] != 4) {
-              score1++;
-              score2--;
+            for(int i = 0; i < baseline; i++){
+              index[i] = 0;
             }
-
-            index[4] = 4;
-            index[9] = 4;
-
-            score1++;
-            score2--;
-          }
-          break;
-        }
-
-        case 5:{
-          if(index[0] != 5) {
-            if(index[5] != 5) {
-              score2++;
-              score1--;
+            for(int i = 20; i > baseline; i--){
+              index[i] = 1;
             }
-
-            index[5] = 5;
-            index[0] = 5;
-
-            score1--;
-            score2++;
+            break;
           }
-          break;
-        }
-        case 6:{
-          if(index[1] != 6) {
-            if(index[6] != 6) {
-              score2++;
-              score1--;
-            }
-
-            index[6] = 6;
-            index[1] = 6;
-
-            score1--;
-            score2++;
-          }
-          break;
-        }
-        case 7:{
-          if(index[2] != 7) {
-            if(index[7] != 7) {
-              score2++;
-              score1--;
-            }
-
-            index[7] = 7;
-            index[2] = 7;
-
-            score1--;
-            score2++;
-          }
-          break;
-        }
-        case 8:{
-          if(index[3] != 8) {
-            if(index[8] != 8) {
-              score2++;
-              score1--;
-            }
-
-            index[8] = 8;
-            index[3] = 8;
-
-            score1--;
-            score2++;
-          }
-          break;
-        }
-        case 9:{
-          if(index[4] != 9) {
-            if(index[9] != 9) {
-              score2++;
-              score1--;
-            }
-
-            index[9] = 9;
-            index[4] = 9;
-
-            score1--;
-            score2++;
-          }
-          break;
-        }
       }
+      print('start_flag = $start_flag\n');
     });
+  }
+
+// 시간 초과되면 colorchange해주자.
+  void timeOut(){
+    if(score1 > score2) print('Winner: Player Red');
+    else if(score2 > score1) print('Winner: Player Blue');
+    else print('draw');
   }
 }
