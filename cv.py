35dbb565c05c2ad8d6b06eb2a7b4713cdbcaeb73
lmh <minhyeok.lee1@gmail.com>
2025-06-24 08:52:47 +0900

    chore: 🔥 Remove duplicated root-level files after merge

--- cv.py
+++
@@ -1,7 +0,0 @@
-import cv2 
-
-url = 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov'
-
-cap = cv2.VideoCapture(url)
-while True : ret, frame = cap.read()
-cv2.imshow("video", frame) cv2.waitKey(1)
